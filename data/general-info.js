b2616c96d020622cc0fd0f3987799def82cda57a
mprhode <matilda.rhode@gmail.com>
2024-08-22 15:41:45 +0100

    Add files via upload

---
+++ data/general-info.js
@@ -0,0 +1,39 @@
+const infoData = [
+    {
+    "title": "This is an AI model",
+    "description": "This model using a machine learning algorithm, sometimes referred to as AI, it is trained on public data to automatically generate images from your inputs."
+},
+{
+    "title": "Training data",
+    "description": "The training data used was compiled from publicly available sources and then hand-labelled and filtered by AmazingAI employees."
+},
+{   "title": "How your data is used",
+    "description": "Your prompts are stored for our AI logging protocol to support contesatbility"
+},
+{
+    "title": "Model limitations",
+    "description": "The model is not good at generating text in images, especially long words."
+},
+{
+    "title": "What good looks like",
+    "description": "This model should generate images that closely mirror what you have described. Test prompts which we know work with normal peformance are: 'An Astronaut riding a horse', 'A baby skydiving'"
+},
+{   "title": "Contestability process",
+    "description": "You can choose who to submit your contest to - follow ContestAI below for more one the process"},
+{   "title": "Independence",
+    "description": "Our independent ethics board has the power to delay, question and block decisions and actions. It's members are A, B, C, and D."},
+{   "title": "Principles", 
+    "description": "This app was developed in line with our 4 core AI principles of: transparency, robustness, sustainability and data governance/privacy"},
+{   "title": "Sustainability",
+    "description": "This app was trained using 100% renewable energy sources using about 2000MWh. Each model query costs about 0.0003KWh"},
+{   "title": "Continuous Improvement",
+    "description": "This app is reviewed every 3 months and improved thanks to feedback (contests) from users, read our contestability update for more information."
+},
+{   "title": "AI Security",
+    "details": "This app is regularly tested for security flaws and follows international best practices for securing AI models."
+},
+{   "title": "Supply chain", 
+    "details": "This app uses AmazingAI's own model fine-tuned from an open-source RoBERTa implementation."},
+    {"title": "Contestability",
+    "description": "You have the right to contest any output of our models. To find out more about the scope of contests that we are able to process and resolve, please see the contestAI page using the button at the bottom of the screen" }
+]

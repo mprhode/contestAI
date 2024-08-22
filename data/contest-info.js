1f4a3cef72f9e0d14207db5e8ab7a26460a821ac
mprhode <matilda.rhode@gmail.com>
2024-08-22 15:41:23 +0100

    Update contest-info.js

--- data/contest-info.js
+++ data/contest-info.js
@@ -1,1 +1,19 @@
-
+const infoData = [{
+    "title": "What can I contest?",
+    "description": "We will review contested outputs that you believe to be inaccurate (disinformation), discriminatory, a privacy violation. Contests are subjective and you may not get the outcome you are hoping for but we appreciate your time to feedback."
+},
+{
+    "title": "Who will recieve my contest?",
+    "description": "Your contest will be shared directly with us and an independent third-party for analysis."
+},
+{   "title": "How long will it take to get a response?",
+    "description": "Your claim will be processed automatically and you can choose to 'accept' the automated response or send for human analysis."
+},
+{
+    "title": "What outcome can I expect?",
+    "description": "If your query does not fall within our criteria we will explain why and you will have the option to connect with a contestability expert to discuss your claim further. All claims are anonymised so please look to our periodic report for more information"
+},
+   { "title": "Quartely contest report",
+    "description": "read about recent changes we have made in response to contests in our regular report."
+}
+]
